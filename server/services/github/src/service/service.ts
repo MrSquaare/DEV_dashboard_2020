c83441db02723b9147f1f53ae7b4719b74684fec
Guillaume Bonnet <guillaume.bonnet@epitech.eu>
2020-11-26 11:25:55 +0100

    feat: Update @dashboard/service-github

--- server/services/github/src/service/service.ts
+++ server/services/github/src/service/service.ts
@@ -1,6 +1,10 @@
 import { OAuth2Options } from "@dashboard/oauth";
 import { ServiceAction, ServiceOAuth2 } from "@dashboard/service";
-import { GitHubUserAction } from "../actions";
+import {
+    GitHubFollowersAction,
+    GitHubFollowingAction,
+    GitHubUserAction,
+} from "../actions";
 import { githubClientId, githubClientSecret } from "../constants";
 
 export class GitHubService extends ServiceOAuth2 {
@@ -8,7 +12,11 @@ export class GitHubService extends ServiceOAuth2 {
     readonly name = "GitHub";
     readonly description = "GitHub service";
     readonly version = "1.0.0";
-    readonly actions: ServiceAction[] = [new GitHubUserAction()];
+    readonly actions: ServiceAction[] = [
+        new GitHubFollowersAction(),
+        new GitHubFollowingAction(),
+        new GitHubUserAction(),
+    ];
     readonly options: OAuth2Options = {
         authorizationURL: "https://github.com/login/oauth/authorize",
         tokenURL: "https://github.com/login/oauth/access_token",
