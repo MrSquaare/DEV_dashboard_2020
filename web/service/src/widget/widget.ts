fbc5efb7f9a7669e9d3538313df47bceed296e9d
Guillaume Bonnet <guillaume.bonnet@epitech.eu>
2020-11-28 20:52:42 +0100

    feat: Update @dashboard-web/service

--- web/service/src/widget/widget.ts
+++
@@ -1,8 +0,0 @@
-export abstract class Widget {
-    abstract readonly id: string;
-    abstract readonly name: string;
-    abstract readonly description: string;
-    abstract readonly actionId: string;
-
-    abstract create(instance: string): JSX.Element;
-}
