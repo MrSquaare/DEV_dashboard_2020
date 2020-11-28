de9db2edea727e796ec6937a51b63b4813863c1f
Guillaume Bonnet <guillaume.bonnet@epitech.eu>
2020-11-28 23:10:00 +0100

    feat: Update @dashboard-web/app

--- web/app/src/index.ts
+++ web/app/src/index.ts
@@ -1,9 +1,10 @@
 import { Configuration, Core } from "@dashboard-web/core";
 import GitHubService from "@dashboard-web/service-github";
 import TwitterService from "@dashboard-web/service-twitter";
+import WeatherService from "@dashboard-web/service-weather";
 
 const configuration: Configuration = {
-    services: [GitHubService, TwitterService],
+    services: [GitHubService, TwitterService, WeatherService],
 };
 const core = new Core(configuration);
 
