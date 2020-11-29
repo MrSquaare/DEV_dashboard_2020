b06e0cab9d82c41f19cad04e8e23563509630754
Guillaume Bonnet <28934649+MrSquaare@users.noreply.github.com>
2020-11-29 14:33:23 +0100

    feat: Add server redirect variables

--- web/app/src/constants/environment.ts
+++ web/app/src/constants/environment.ts
@@ -8,3 +8,16 @@ export const serverHostname = process.env.SERVER_HOSTNAME || "localhost";
 export const serverPort = process.env.SERVER_PORT || 8080;
 export const serverHost =
     serverProtocol + "://" + serverHostname + ":" + serverPort;
+
+export const serverRedirectProtocol =
+    process.env.SERVER_REDIRECT_PROTOCOL || serverProtocol;
+export const serverRedirectHostname =
+    process.env.SERVER_REDIRECT_HOSTNAME || serverHostname;
+export const serverRedirectPort =
+    process.env.SERVER_REDIRECT_PORT || serverPort;
+export const serverRedirectHost =
+    serverRedirectProtocol +
+    "://" +
+    serverRedirectHostname +
+    ":" +
+    serverRedirectPort;
